1a87042bafd316c884774b39a8d2008fa393e744
Jiocolon313 <399874@colum.edu>
2021-10-18 17:53:04 -0500

    World Interactions
    World Interactions for 2D game

--- 2D- Begginner Tutorial/Assets/Scripts/RubyController.cs
+++ 2D- Begginner Tutorial/Assets/Scripts/RubyController.cs
@@ -1,22 +1,69 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //vector positions where changed from 3 to 4 to test different speeds
+    public float speed = 4.0f;
+
+    public int maxHealth = 10;
+    public float timeInvincible = 2.0f;
+    //rubys health.
+    int currentHealth;
+    public int health { get { return currentHealth; } }
+
+    bool isInvincible;
+    float invincibleTimer;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    //added start back into the code 
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        // refactored update and moved code around.
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        //this will stop ruby from jittering when moving around
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
-
