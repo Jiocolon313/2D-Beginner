8d101903200679035bb67741b487b817ad7cbc09
Jiocolon313 <399874@colum.edu>
2021-10-24 13:14:01 -0500

    Ruby's Adventure - Sprite Animation, Projectiles  and More
    Ruby's Adventure - Sprite Animation, Projectiles  and More

--- 2D- Begginner Tutorial/Assets/Scripts/RubyController.cs
+++ 2D- Begginner Tutorial/Assets/Scripts/RubyController.cs
@@ -1,18 +1,19 @@
-
+﻿
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    //vector positions where changed from 3 to 4 to test different speeds
-    public float speed = 4.0f;
+    public float speed = 3.0f;
 
-    public int maxHealth = 10;
-    public float timeInvincible = 2.0f;
-    //rubys health.
-    int currentHealth;
+    public int maxHealth = 5;
+
+    public GameObject projectilePrefab;
+
     public int health { get { return currentHealth; } }
+    int currentHealth;
 
+    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
 
@@ -20,19 +21,35 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
-    //added start back into the code 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
         currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // refactored update and moved code around.
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -41,6 +58,10 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
 
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
 
     void FixedUpdate()
@@ -48,7 +69,7 @@ public class RubyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
-        //this will stop ruby from jittering when moving around
+
         rigidbody2d.MovePosition(position);
     }
 
@@ -65,5 +86,15 @@ public class RubyController : MonoBehaviour
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
